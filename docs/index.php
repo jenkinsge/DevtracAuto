de38e5e13c3c42ac9c5f4e000eb82ee71b7670dc
Reinier Battenberg <reinier.battenberg@mountbatten.net>
2014-06-18 18:16:48 +0300

    First version of documentation that is going to be shown on api.devtrac.org

---
+++ docs/index.php
@@ -0,0 +1,26 @@
+<?php
+
+/** @mainpage Developer Documentation
+ * Welcome to the Devtrac developer's documentation. This documentation
+ * is mainly generated from the code that runs Devtrac and includes 
+ * the Devtrac installation profile, the custom modules, the exported features
+ * as well as the documentation of all the contrib modules that are used by a
+ * plain Devtrac install.
+ * 
+ *
+ * - A few components of Devtrac
+ *   - @link api Devtrac API @endlink
+ *   - @link database Database abstraction layer @endlink
+ *   - @link menu Menu system @endlink
+ *   - @link form_api Form generation @endlink
+ *   - @link file File upload system @endlink
+ *   - @link field Field API @endlink
+ *   - @link search Search system @endlink
+ *   - @link node_access Node access system @endlink
+ *   - @link themeable Theme system @endlink
+ *   - @link /api/constants/7 Constants @endlink
+ *   - @link /api/globals/7 Global variables @endlink
+ * - @link examples.index.php Example modules @endlink
+ * - In-depth discussions
+ *   - @link forms_api_reference.html Forms API Reference @endlink
+ */
