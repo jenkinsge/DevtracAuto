ffef93c585d47619604815bb83a71409709259d7
Reinier Battenberg <reinier.battenberg@mountbatten.net>
2014-06-19 16:50:00 +0300

    Added additional (experimental) documentation linkages

--- docs/api.php
+++ docs/api.php
@@ -1,8 +1,28 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * @file api.php Contains the documentation about the different APIs
  */
+ /**  
+ * @defgroup devtrac_api Devtrac API
+ * @{
+ * For those who would like to build a mobile application there is an API available
+ * based on the services module. This API is used by the 
+ * @link https://github.com/davidebukali/DevtracMobile Devtrac Mobile @endlink client.
+ *
+ * There is also an API that allowws data to be published into any CKAN installation. CKAN
+ * then  has an API to interacti directly with the data.
+ *
+ * @see devtrac_services_api
+ * @see ckan_api
+ */
+ 
+ /* @defgroup devtrac_services_api Services API
+ * @{
+ * If you want to build an application that interacts with Devtrac in a very direct
+  * and authenticated way, this is the API you would like to use. 
+  */
 
+ /* @defgroup devtrac_ckan_api Services API
+ * @{
+ * The CKAN API allows you to harvest Devtrac data into CKAN.
+  */
--- docs/index.php
+++ docs/index.php
@@ -9,18 +9,6 @@
  * 
  *
  * - A few components of Devtrac
- *   - @link api Devtrac API @endlink
- *   - @link database Database abstraction layer @endlink
- *   - @link menu Menu system @endlink
- *   - @link form_api Form generation @endlink
- *   - @link file File upload system @endlink
- *   - @link field Field API @endlink
- *   - @link search Search system @endlink
- *   - @link node_access Node access system @endlink
- *   - @link themeable Theme system @endlink
- *   - @link /api/constants/7 Constants @endlink
- *   - @link /api/globals/7 Global variables @endlink
- * - @link examples.index.php Example modules @endlink
- * - In-depth discussions
- *   - @link forms_api_reference.html Forms API Reference @endlink
+ *   - @link devtrac_api Devtrac API @endlink
+ *   - @link devtrac_documentation About the Documentation @endlink
  */
