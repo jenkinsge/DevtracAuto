ec01f8cbc82d086eba43238e41c38ea525f5e0d9
Reinier Battenberg <reinier.battenberg@mountbatten.net>
2014-07-04 18:10:10 +0300

    Added sections to index page

--- docs/index.php
+++ docs/index.php
@@ -10,30 +10,30 @@
  * The main documentation and user tutorials can be found on 
  * @link the Devtrac website http://www.devtrac.org/node/20 @endlink
  * 
- * Folder structure.
+ * @section main_folders Folder structure 1
  * 
  * The Devtrac profile is located in /profiles/devtrac and has the following 
  * subfolders:
  * 
- * /profiles/devtrac/docs
+ * @subsection main_folders_docs /profiles/devtrac/docs A
  * Contains the main documentation of the profile (including this file). If you 
  * want to cover a bigger Devtrac coding subject, please read  * @link devtrac_documentation About the Documentation @endlink before
  * you add files to this folder. 
  * 
- * /profiles/devtrac/modules/features
+ * @subsection main_folders_features /profiles/devtrac/modules/features B
  * 
  * The Devtrac distribution makes use of the @link features @endlink module. All 
  * nodetypes, views, search_api indices and as many of the configuration as 
  * possible is a feature.
  * You will find all the features in the /profiles/devtrac/modules/features folder
  * 
- * /profiles/devtrac/modules/custom
+ * @subsection main_folders_modules /profiles/devtrac/modules/custom C
  * The modules that are too specific for Devtrac are stored here. Some of them
  * we have not found the time yet to abstract them out to become standalone modules
  * which is our general wish. The heart of the Devtrac application is the @link devtrac @endlink module.
  * It contains menus, node_hook implementations, form_alter etc. 
  * 
- * Some general subject have seperate pages:
+ * @section main_index  Some general subject have seperate pages 2
  *
  * - @link /api/projects All used modules and themes @endlink
  * - @link devtrac_api Devtrac API @endlink
