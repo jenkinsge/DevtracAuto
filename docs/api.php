c23ca24677decf84e9b446660eaf7bf41e6bfbe4
Reinier Battenberg <reinier.battenberg@mountbatten.net>
2014-07-04 17:47:26 +0300

    Documentation updates

--- docs/api.php
+++ docs/api.php
@@ -9,7 +9,7 @@
  * based on the services module. This API is used by the 
  * @link https://github.com/davidebukali/DevtracMobile Devtrac Mobile @endlink client.
  *
- * There is also an API that allowws data to be published into any CKAN installation. CKAN
+ * There is also an API that allows data to be published into any CKAN installation. CKAN
  * then  has an API to interacti directly with the data.
  *
  * @see devtrac_services_api
@@ -19,7 +19,9 @@
  /* @defgroup devtrac_services_api Services API
  * @{
  * If you want to build an application that interacts with Devtrac in a very direct
-  * and authenticated way, this is the API you would like to use. 
+  * and authenticated way, this is the API you would like to use.
+  * 
+  * @see devtrac7_services_api_default_services_endpoint
   */
 
  /* @defgroup devtrac_ckan_api Services API
