1499a84e0984aabd9c9600bf57df1a1fbd09b7fc
Reinier Battenberg <reinier.battenberg@mountbatten.net>
2014-07-04 18:19:59 +0300

    restructured more documentation

--- docs/api.php
+++ docs/api.php
@@ -10,15 +10,9 @@
  * @link https://github.com/davidebukali/DevtracMobile Devtrac Mobile @endlink client.
  *
  * There is also an API that allows data to be published into any CKAN installation. CKAN
- * then  has an API to interacti directly with the data.
+ * then has an API to interact directly with the data.
  *
  * @see devtrac_services_api
- * @see ckan_api
+ * @see devtrac_ckan_api
  *@}
  */
- 
- /* @defgroup devtrac_ckan_api Services API
- * @{
- * The CKAN API allows you to harvest Devtrac data into CKAN.
-  * @}
-  */
