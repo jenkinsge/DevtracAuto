69ab46b49f9e67d2c8998ebc1b5cd50ba0505bba
Dan <dmulindwa@gmail.com>
2014-08-13 11:00:28 +0300

    fix vocabularies in css

--- scripts/textreplace.sh
+++ scripts/textreplace.sh
@@ -27,3 +27,39 @@
  grep -rl 'vocabulary_9' ../modules | xargs sed -i 's/vocabulary_9/purpose/g'
  grep -rl 'vocabulary_9' ../test  | xargs sed -i 's/vocabulary_9/purpose/g'
  grep -rl 'vocabulary_9' ../themes  | xargs sed -i 's/vocabulary_9/purpose/g'
+ 
+
+ grep -rl 'vocabulary-1' ../modules | xargs sed -i 's/vocabulary-1/location-type/g'
+ grep -rl 'vocabulary-1' ../test | xargs sed -i 's/vocabulary-1/location-type/g'
+ grep -rl 'vocabulary-1' ../themes | xargs sed -i 's/vocabulary-1/location-type/g'
+
+ grep -rl 'vocabulary-4' ../modules | xargs sed -i 's/vocabulary-4/department/g'
+ grep -rl 'vocabulary-4' ../test | xargs sed -i 's/vocabulary-4/department/g'
+  grep -rl 'vocabulary-4' ../themes | xargs sed -i 's/vocabulary-4/department/g'
+
+ grep -rl 'vocabulary-5' ../modules | xargs sed -i 's/vocabulary-5/function/g'
+ grep -rl 'vocabulary-5' ../test | xargs sed -i 's/vocabulary-5/function/g'
+ grep -rl 'vocabulary-5' ../themes | xargs sed -i 's/vocabulary-5/function/g'
+
+ grep -rl 'vocabulary-6' ../modules | xargs sed -i 's/vocabulary-6/distict/g'
+ grep -rl 'vocabulary-6' ../test  | xargs sed -i 's/vocabulary-6/district/g'
+ grep -rl 'vocabulary-6' ../themes  | xargs sed -i 's/vocabulary-6/district/g'
+
+ grep -rl 'vocabulary-7' ../modules | xargs sed -i 's/vocabulary-7/site-report-type/g'
+ grep -rl 'vocabulary-7' ../test  | xargs sed -i 's/vocabulary-7/site-report-type/g'
+ grep -rl 'vocabulary-7' ../themes  | xargs sed -i 's/vocabulary-7/site-report-type/g'
+
+ grep -rl 'vocabulary-8' ../modules | xargs sed -i 's/vocabulary-8/oecd-sector-code/g'
+ grep -rl 'vocabulary-8' ../test  | xargs sed -i 's/vocabulary-8/oecd-sector-code/g'
+ grep -rl 'vocabulary-8' ../themes  | xargs sed -i 's/vocabulary-8/oecd-sector-code/g'
+
+ grep -rl 'vocabulary-9' ../modules | xargs sed -i 's/vocabulary-9/purpose/g'
+ grep -rl 'vocabulary-9' ../test  | xargs sed -i 's/vocabulary-9/purpose/g'
+ grep -rl 'vocabulary-9' ../themes  | xargs sed -i 's/vocabulary-9/purpose/g'
+ 
+ 
+ 
+ 
+ 
+ 
+ 
