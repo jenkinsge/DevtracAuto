4a082e7bf0a40ef9ed79f96305e77f9923ce960e
Joseph Ssekono <ssekono@gmail.com>
2015-01-05 17:29:01 +0300

    Issue #1987: Overrode a tpl file function to add a space and a comma after every listed admin unit

--- themes/devtrac_classic/template.php
+++ themes/devtrac_classic/template.php
@@ -1,2 +1,25 @@
 <?php
 
+function devtrac_classic_field__taxonomy_vocabulary_6__fieldtrip($variables) {
+
+  $output = '';
+
+  // Render the label, if it's not hidden.
+  if (!$variables['label_hidden']) {
+    $output .= '<div class="field-label"' . $variables['title_attributes'] . '>' . $variables['label'] . ':&nbsp;</div>';
+  }
+
+  // Render the items.
+  $output .= '<div class="field-items"' . $variables['content_attributes'] . '>';
+  foreach ($variables['items'] as $delta => $item) {
+    $classes = 'field-item ' . ($delta % 2 ? 'odd' : 'even');
+    $output .= '<div class="' . $classes . '"' . $variables['item_attributes'][$delta] . '>' . drupal_render($item) . '</div>, ';
+  }
+  $output .= '</div>';
+
+  // Render the top-level DIV.
+  $output = '<div class="' . $variables['classes'] . '"' . $variables['attributes'] . '>' . $output . '</div>';
+
+  return $output;
+
+}
